329a0b23c366b54e08380b5303b1a3efb53bef8f
ddsha441981 <ddsa44191@gmail.com>
2020-12-14 16:31:46 +0000

    List and Tuple Chapter....with Practice...

---
+++ chapter_04_Lists_Tuples/list_example.py
@@ -0,0 +1,20 @@
+# Author: Deendayal Kumawat
+""" Date: 11/12/19
+Descriptions: List And Tuples """
+# List is a just like container which store set of value of any data type
+# Create a list using []
+print("*********List and Tupples*********")
+# Access using index using a[0], a[1], a[2]
+abcList = [7, 9, "Abc", "aaa", "a", 4, "Hello"]
+print(abcList)
+print(abcList[0])
+print(abcList[6])
+# print(abcList[7])  # Error list index out of range
+
+# Change the value of list using
+abcList[0] = 16
+print(abcList)
+
+# We can create a list with items of different types
+a = [False,True,45.025,75,"Abc","xyz"]
+print(a)
---
+++ chapter_04_Lists_Tuples/list_slicing.py
@@ -0,0 +1,14 @@
+# Author: Deendayal Kumawat
+""" Date: 11/12/19
+Descriptions: List And Tuples """
+# List is a just like container which store set of value of any data type
+# Create a list using []
+print("*********List and Tupples*********")
+
+abc = ["a","j8","57","86","aa5","s23","aw1","f",]
+print(abc)
+print(abc[0:5])
+print(abc[4:8])
+
+print(abc[0:-4])
+print(abc[-4])
